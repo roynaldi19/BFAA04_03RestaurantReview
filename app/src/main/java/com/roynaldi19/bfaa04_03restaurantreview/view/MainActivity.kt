d0b40b952d1a18f15936158bdb60483fb1ace433
roynaldi19 <roynaldi19@gmail.com>
2022-02-01 14:39:15 +0700

    update KTX dengan MainActivity

---
+++ app/src/main/java/com/roynaldi19/bfaa04_03restaurantreview/view/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.roynaldi19.bfaa04_03restaurantreview.view
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import com.roynaldi19.bfaa04_03restaurantreview.CustomerReviewsItem
+import com.roynaldi19.bfaa04_03restaurantreview.Restaurant
+import com.roynaldi19.bfaa04_03restaurantreview.adapter.ReviewAdapter
+import com.roynaldi19.bfaa04_03restaurantreview.databinding.ActivityMainBinding
+import com.roynaldi19.bfaa04_03restaurantreview.viewModel.MainViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        mainViewModel.restaurant.observe(this, { restaurant ->
+            setRestaurantData(restaurant)
+
+        })
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvReview.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.rvReview.addItemDecoration(itemDecoration)
+
+        mainViewModel.listReview.observe(this, { consumerReviews ->
+            setReviewData(consumerReviews)
+
+        })
+
+        mainViewModel.isLoading.observe(this, {
+            showLoading(it)
+
+        })
+
+        mainViewModel.snackbarText.observe(this, {
+            it.getContentIfNotHandled()?.let {
+                Snackbar.make(
+                    window.decorView.rootView, it, Snackbar.LENGTH_SHORT
+                ).show()
+
+            }
+        })
+
+        binding.btnSend.setOnClickListener { view ->
+            mainViewModel.postReview(binding.edReview.text.toString())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+        }
+    }
+
+    private fun setRestaurantData(restaurant: Restaurant) {
+        binding.tvTitle.text = restaurant.name
+        binding.tvDescription.text = restaurant.description
+        Glide.with(this@MainActivity)
+            .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+            .into(binding.ivPicture)
+
+    }
+
+    private fun setReviewData(consumerReviews: List<CustomerReviewsItem>) {
+        val listReview = ArrayList<String>()
+        for (review in consumerReviews) {
+            listReview.add(
+                """
+                ${review.review}
+                - ${review.name}
+                """.trimIndent()
+
+            )
+        }
+
+        val adapter = ReviewAdapter(listReview)
+        binding.rvReview.adapter = adapter
+        binding.edReview.setText("")
+
+    }
+
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+
+    }
+}
