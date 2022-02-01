0645b0fb4f426ccb1f604a5092de62d21b07fec8
roynaldi19 <roynaldi19@gmail.com>
2022-02-01 14:25:15 +0700

    update singleEvent snackBar di bungkus Event

--- app/src/main/java/com/roynaldi19/bfaa04_03restaurantreview/viewModel/MainViewModel.kt
+++ app/src/main/java/com/roynaldi19/bfaa04_03restaurantreview/viewModel/MainViewModel.kt
@@ -4,10 +4,8 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.roynaldi19.bfaa04_03restaurantreview.CustomerReviewsItem
-import com.roynaldi19.bfaa04_03restaurantreview.PostReviewResponse
-import com.roynaldi19.bfaa04_03restaurantreview.Restaurant
-import com.roynaldi19.bfaa04_03restaurantreview.RestaurantResponse
+import com.roynaldi19.bfaa04_03restaurantreview.*
+import com.roynaldi19.bfaa04_03restaurantreview.Event.Event
 import com.roynaldi19.bfaa04_03restaurantreview.api.ApiConfig
 import retrofit2.Call
 import retrofit2.Callback
@@ -24,8 +22,8 @@ class MainViewModel : ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
 
-    private val _snackbarText = MutableLiveData<String>()
-    val snackbarText: LiveData<String> = _snackbarText
+    private val _snackbarText = MutableLiveData<Event<String>>()
+    val snackbarText: LiveData<Event<String>> = _snackbarText
 
     companion object {
         private const val TAG = "MainViewModel"
@@ -71,7 +69,7 @@ class MainViewModel : ViewModel() {
                 _isLoading.value = false
                 if (response.isSuccessful) {
                     _listReview.value = response.body()?.customerReviews
-                    _snackbarText.value = response.body()?.message
+                    _snackbarText.value = Event(response.body()?.message.toString())
                 } else {
                     Log.e(TAG, "onFailure: ${response.message()}")
                 }
